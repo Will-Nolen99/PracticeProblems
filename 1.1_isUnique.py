349006ee68d139243c3c413c860c3d055bd33a06
Will-Nolen99 <wpncomps@yahoo.com>
2021-09-01 20:10:29 -0400

    1.1 No dict done

--- 1.1_isUnique.py
+++ 1.1_isUnique.py
@@ -12,10 +12,27 @@ def is_unique(word):
     return True
 
 
+# O(n) where n is length of word
+def is_unique_no_dict(word):
+    ASCII_LIMIT = 128 # number of total ascii values
+    possible_chars = [False for _ in range(ASCII_LIMIT)]  # each index in array will represent a single ascii value
+
+    for letter in word:  # Iterate through word
+        value = ord(letter)
+        if not possible_chars[value]: 
+            possible_chars[value] = True
+        else:
+            return False
+
+    return True
 
 
+print("dict")
 words = ["hello", "world", "push", "pop"]
 
 for w in words:
     print(is_unique(w))
 
+print("No dict")
+for w in words:
+    print(is_unique_no_dict(w))
